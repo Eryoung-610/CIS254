0e0ae81b349ef14aa3c1dc5e0b29e39d2b28948f
Young <Young@DESKTOP-7E11H84>
2020-03-30 03:26:53 -0700

    Assignment 1

---
+++ Assignment1/src/main.java
@@ -0,0 +1,42 @@
+/*
+ Eric Young
+ CIS254
+ January 14th, 2019
+ Professor Harden
+ Assignment 1.1
+ 
+ This program fulfills the requirements for assignment 1.1 which requires several statements that print out the same sentence being, "Tran says this is her first computer program." 
+ The problem requires there to be either one statement to print the sentence or multiple statements to print the sentence.
+ */
+
+public class main {
+	public static void main(String[]args) {
+		
+		
+		//1.1A
+		System.out.println("Tran says this is her first computer program.");
+		
+		//1.1B
+		System.out.println("Tran says this is her");
+		System.out.println("first computer program");
+		
+		//1.1C
+		System.out.print("Tran says this is her \nfirst computer program.");
+		
+		System.out.println(" ");
+		
+		//1.1D
+		System.out.print("Tran says this is");
+		System.out.print(" her first computer program.");
+	}
+}
+
+/*
+Tran says this is her first computer program.
+Tran says this is her
+first computer program
+Tran says this is her 
+first computer program. 
+Tran says this is her first computer program.
+*/
+
